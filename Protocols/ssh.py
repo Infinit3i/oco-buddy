a07b0bc3f21301a99d83fd465f4be256d75cd9f5
infinit3i <matthewiver@protonmail.com>
2024-12-30 16:39:15 -0500

    update ssh

--- Protocols/ssh.py
+++ Protocols/ssh.py
@@ -5,6 +5,9 @@ def notes(target_ip, open_ports):
     title = "Notes"
     content = f"""
 HTTP Notes:
+  - sudo -l
+    - gtfo bins
+  - id
   - find / -uid <USER UUID> -type f -ls 2>/dev/null | grep -v '/proc*'
   - find / -name 'root.txt' -exec cat {{}} \\; 2>/dev/null
   - find / -perm -4000 -o -perm -2000 -ls 2>/dev/null
