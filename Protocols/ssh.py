e77787b88b3920821ccf74a6caea8ad53a2957a1
infinit3i <matthewiver@protonmail.com>
2024-12-30 16:38:21 -0500

    all ssh is on the box

--- Protocols/ssh.py
+++ Protocols/ssh.py
@@ -1,34 +1,15 @@
 from Modules.Imports.protocol_imports import *
 
-def find_user_files(target_ip, open_ports):
-    title = "Find Files Owned by User"
-    content = f"find / -uid <USER UUID> -type f -ls 2>/dev/null | grep -v '/proc*'"
-    run_command(title, content, target_ip, open_ports)
 
-def find_root_file(target_ip, open_ports):
-    title = "Find 'root.txt' File"
-    content = f"find / -name 'root.txt' -exec cat {{}} \\; 2>/dev/null"
-    run_command(title, content, target_ip, open_ports)
-
-def list_setuid_files(target_ip, open_ports):
-    title = "List SETUID/SETGID Files"
-    content = f"find / -perm -4000 -o -perm -2000 -ls 2>/dev/null"
-    run_command(title, content, target_ip, open_ports)
-
-def check_writable_files(target_ip, open_ports):
-    title = "Check Writable Config Files"
-    content = f"find /etc -perm -2"
-    run_command(title, content, target_ip, open_ports)
-
-def grep_passwords(target_ip, open_ports):
-    title = "Grep for Passwords in /etc/"
-    content = f"grep -Inri passw /etc/* 2>/dev/null"
-    run_command(title, content, target_ip, open_ports)
-
-def lin_enum(target_ip, open_ports):
-    title = "Run LinEnum.sh"
-    content = (
-        f"Download and execute LinEnum.sh from:\n"
-        f"https://github.com/rebootuser/LinEnum\n\n"
-    )
+def notes(target_ip, open_ports):
+    title = "Notes"
+    content = f"""
+HTTP Notes:
+  - find / -uid <USER UUID> -type f -ls 2>/dev/null | grep -v '/proc*'
+  - find / -name 'root.txt' -exec cat {{}} \\; 2>/dev/null
+  - find / -perm -4000 -o -perm -2000 -ls 2>/dev/null
+  - find /etc -perm -2
+  - grep -Inri passw /etc/* 2>/dev/null
+  - https://github.com/rebootuser/LinEnum
+"""
     run_command(title, content, target_ip, open_ports)
